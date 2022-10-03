a75d109da2516ad5574e0b53ecbd00beadbe240a
CookiePieWw <1035325592@qq.com>
2022-10-03 19:13:10 +0800

    fix: fixed lexer bugs

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -24,21 +24,24 @@ namespace lexer {
 		// "/*abcd*/" will be identified as ""
 		std::stack<int> s;
 		for (std::size_t i = 0; i < src.size(); ++i) { // task: implement a pythonic enumerate in C++
-			SrcType::value_type line = src[i];
+			SrcType::value_type &line = src[i];
 			if (line.empty()) {
 				com[i] = true;
 				continue;
 			}
 			for (std::size_t j = 0; j < line.size() - 1; ++j) {
-				if (s.empty() && line[j] == '\\' && line[j+1] == '*') {
+				if (s.empty() && line[j] == '/' && line[j+1] == '*') {
 					s.emplace(i);
 					line.erase(line.begin() + j, line.end());
+					break;
 				}
-				if (!s.empty() && line[j] == '*' && line[j+1] == '\\') {
-					for (std::size_t k = s.top() + 1; k < i - 1; ++k) {
-						com[i] = true;
+				if (!s.empty() && line[j] == '*' && line[j+1] == '/') {
+					for (std::size_t k = s.top() + 1; k <= i - 1; ++k) {
+						com[k] = true;
 					}
+					s.pop();
 					line.erase(line.begin(), line.begin() + j + 2);
+					break;
 				}
 			}
 		}
